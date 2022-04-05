4a9dca9f01cb1afb8fc10c081cbe7a6edacc2e80
hgs-study <hgstudy_@naver.com>
2022-04-05 21:39:52 +0900

    Initial commit

---
+++ src/main/java/com/pir/PirApplication.java
@@ -0,0 +1,13 @@
+package com.pir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pir/PirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
